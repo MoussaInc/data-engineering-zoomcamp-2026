00a33b1b76d421daee1ad145a8bd63063047eac5
mballo <papis.mballo@gmail.com>
2026-03-11 22:33:27 +0100

    script batch to download data

--- homework/M07-batch/notebooks/download_data.sh
+++ homework/M07-batch/notebooks/download_data.sh
@@ -11,12 +11,12 @@ for MONTH in {1..12}; do
 
   URL="${URL_PREFIX}/${TAXI_TYPE}/${TAXI_TYPE}_tripdata_${YEAR}-${FMONTH}.csv.gz"
 
-  LOCAL_PREFIX="data/raw/${TAXI_TYPE}/${YEAR}/${FMONTH}"
-  LOCAL_FILE="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv.gz"
-  LOCAL_PATH="${LOCAL_PREFIX}/${LOCAL_FILE}"
+  LOCAL_DIR="data/raw/${TAXI_TYPE}/${YEAR}"
+  FILE_NAME="${TAXI_TYPE}_tripdata_${YEAR}_${FMONTH}.csv.gz"
+  LOCAL_PATH="${LOCAL_DIR}/${FILE_NAME}"
 
   echo "downloading ${URL} to ${LOCAL_PATH}"
-  mkdir -p ${LOCAL_PREFIX}
+  mkdir -p ${LOCAL_DIR}
   wget ${URL} -O ${LOCAL_PATH}
 
 done
